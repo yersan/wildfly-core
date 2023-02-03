acc567fa8e7d8fe005a5e33ee32dac52f1411dc8
Yeray Borges <yborgess@redhat.com>
2023-02-03 11:34:04 +0000

    Windows Script

--- core-feature-pack/common/src/main/resources/content/bin/installation-manager.bat
+++ core-feature-pack/common/src/main/resources/content/bin/installation-manager.bat
@@ -1,14 +1,21 @@
 @echo off
 :: Invokes the installation manager tool to apply the prepared server.
 
-set INSTALLATION_HOME=%1
-set WORK_DIR=%2
+set INSTALLATION_HOME=%~1
+set WORK_DIR=%~2
+set PREPARED_INSTALLATION=%WORK_DIR%\installation-manager\prepared-server
 
-"%INSTALLATION_HOME%/bin/prospero.sh" update apply --dir="%INSTALLATION_HOME%" --update-dir="%WORK_DIR%/installation-manager/prepared-server"
+if not exist "%PREPARED_INSTALLATION%" (
+	echo INFO: There is no a Candidate Server prepared.
+	
+	goto EOF
+)
+
+call "%INSTALLATION_HOME%\bin\prospero.bat" update apply --dir="%INSTALLATION_HOME%" --update-dir="%PREPARED_INSTALLATION%"
 
 if %errorlevel% equ 0 (
     echo INFO: The Candidate Server was successfully applied.
-    del /F /Q "%WORK_DIR%/installation-manager/prepared-server"
+    rmdir /S /Q "%PREPARED_INSTALLATION%"
 
     exit /B 0
 )
@@ -26,4 +33,6 @@ if %errorlevel% equ 2 (
 echo ERROR: An unknown error occurred during the execution of the installation manager.
 
 exit /B 3
+
+:EOF
 
