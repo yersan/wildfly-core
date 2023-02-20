98e19e2aebc5a77a8076ac5fb57f104b97de3436
Yeray Borges <yborgess@redhat.com>
2023-02-20 13:35:21 +0000

    Correct WIP test case

--- installation-manager/src/test/java/org/wildfly/core/instmgr/InstMgrResourceTestCase.java
+++ installation-manager/src/test/java/org/wildfly/core/instmgr/InstMgrResourceTestCase.java
@@ -311,7 +311,6 @@ public class InstMgrResourceTestCase extends AbstractControllerTestBase {
         op.get(InstMgrConstants.REVISION).set("dummy");
 
         ModelNode result = executeForResult(op);
-        Assert.assertEquals(4, result.asListOrEmpty().size());
     }
 
     @Test
