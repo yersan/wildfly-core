d13f26310178267db752786972bc5ad5d956107c
Yeray Borges <yborgess@redhat.com>
2024-10-21 16:05:12 +0100

    [WFCORE-6935] Review launch.sh, add the ability to confirm the changes and allow specify areguments in any order

--- core-feature-pack/common/src/main/resources/content/bin/systemd/generate_systemd_unit.sh
+++ core-feature-pack/common/src/main/resources/content/bin/systemd/generate_systemd_unit.sh
@@ -4,34 +4,88 @@
 # assuming that the server home will be the one that hold this script file.
 # It also allows to specify the user and group that will run the server.
 
+function print_usage() {
+  echo "INFO: Usage: "
+  echo "${SCRIPT_NAME} -m,--mode standalone|domain [-u,--user user] [-g,--group group] [-c,--confirm y|n]"
+
+  exit 1
+}
+
 SCRIPT_NAME=$(basename "$0")
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 JBOSS_HOME="$(realpath "${SCRIPT_DIR}/../..")"
 JBOSS_SYSTEMD_MODE="${1}"
 
-if [ -z "${JBOSS_SYSTEMD_MODE}" ]; then
-  echo "INFO: Usage: "
-  echo "${SCRIPT_NAME} standalone|domain [user name] [group name]"
-  exit 0;
+JBOSS_SYSTEMD_MODE=""
+JBOSS_SYSTEMD_USER="wildfly"
+JBOSS_SYSTEMD_GROUP="wildfly"
+JBOSS_CONFIRM="n"
+
+while [ "$#" -gt 0 ]
+do
+# For each argument option, checks whether the argument length is zero or starts with hyphen(s)
+    case "$1" in
+        -m|--mode)
+            if [ -z "$2" ] || [ ! "$2" = `echo "$2" | sed 's/^-*//'` ]; then
+                echo "Error: Missing argument for $1" >&2
+                print_usage
+            fi
+            JBOSS_SYSTEMD_MODE="$2"
+            shift 2
+            ;;
+        -u|--user)
+            if [ -z "$2" ] || [ ! "$2" = `echo "$2" | sed 's/^-*//'` ]; then
+                echo "Error: Missing argument for $1" >&2
+                print_usage
+            fi
+            JBOSS_SYSTEMD_USER="$2"
+            shift 2
+            ;;
+        -g|--group)
+            if [ -z "$2" ] || [ ! "$2" = `echo "$2" | sed 's/^-*//'` ]; then
+                echo "Error: Missing argument for $1" >&2
+                print_usage
+            fi
+            JBOSS_SYSTEMD_GROUP="$2"
+            shift 2
+            ;;
+        -c|--confirm)
+            if [ -z "$2" ] || [ ! "$2" = `echo "$2" | sed 's/^-*//'` ]; then
+                echo "Error: Missing argument for $1" >&2
+                print_usage
+            fi
+            JBOSS_CONFIRM="$2"
+            shift 2
+            ;;
+        *)
+            echo "Error: Unknown argument $1" >&2
+            print_usage
+            ;;
+    esac
+done
+
+if [ "${JBOSS_SYSTEMD_MODE}" != "standalone" ] && [ "${JBOSS_SYSTEMD_MODE}" != "domain" ]; then
+  print_usage
 fi
 
 SYSTEMD_FILE="${SCRIPT_DIR}/wildfly-${JBOSS_SYSTEMD_MODE}.service"
-JBOSS_SYSTEMD_USER="${2:-wildfly}"
-JBOSS_SYSTEMD_GROUP="${3:-wildfly}"
 
 echo "INFO: systemd unit file to generate: ${SYSTEMD_FILE}"
 echo "INFO: Using JBOSS_HOME: ${JBOSS_HOME}"
 echo "INFO: User: ${JBOSS_SYSTEMD_USER}"
 echo "INFO: Group: ${JBOSS_SYSTEMD_GROUP}"
+echo "INFO: Confirm: ${JBOSS_CONFIRM}"
 
-while true; do
-  read -p "Do you want to generate ${SYSTEMD_FILE}? (y/n): " choice
-  case "$choice" in
-      y|Y ) break;;
-      n|N ) echo "Operation cancelled."; exit 1;;
-      * ) ;;
-  esac
-done
+if [ 'y' != "${JBOSS_CONFIRM}" ] && [ 'Y' != "${JBOSS_CONFIRM}" ];then
+  while true; do
+    read -p "Do you want to generate ${SYSTEMD_FILE}? (y/n): " choice
+    case "$choice" in
+        y|Y ) break;;
+        n|N ) echo "Operation cancelled."; exit 1;;
+        * ) ;;
+    esac
+  done
+fi
 
 sed -e "s|@@@JBOSS_SYSTEMD_SERVER_HOME@@@|${JBOSS_HOME}|g" \
     -e "s|@@@JBOSS_SYSTEMD_USER@@@|${JBOSS_SYSTEMD_USER}|g" \
@@ -47,7 +101,7 @@ echo "INFO: Use ${JBOSS_HOME}/bin/systemd/wildfly-${JBOSS_SYSTEMD_MODE}.conf to 
 echo "INFO: After configuring your environment, to install the server as a systemd service do the following:"
 echo ""
 echo "sudo cp ${SYSTEMD_FILE} $(pkg-config systemd --variable=systemdsystemunitdir)"
-echo "sudo cp ${JBOSS_HOME}/bin/systemd/wildfly-${JBOSS_SYSTEMD_MODE}.conf /etc/sysconfig/wildfly-${JBOSS_SYSTEMD_MODE}.conf"
+echo "sudo cp ${JBOSS_HOME}/bin/systemd/wildfly-${JBOSS_SYSTEMD_MODE}.conf /etc/sysconfig"
 echo "sudo systemctl daemon-reload"
 echo "sudo systemctl start $(basename "${SYSTEMD_FILE}")"
 echo ""
