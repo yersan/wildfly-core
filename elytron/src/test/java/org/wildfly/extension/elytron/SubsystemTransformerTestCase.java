bbb2da68d0c8a2dd96df83f02636c7fdceaf8fa6
Paul Ferraro <paul.ferraro@redhat.com>
2024-12-16 15:40:50 +0000

    Parameterise elytron subsystem transformer test to validate EAP8 and WF31 transformations.

--- elytron/src/test/java/org/wildfly/extension/elytron/SubsystemTransformerTestCase.java
+++ elytron/src/test/java/org/wildfly/extension/elytron/SubsystemTransformerTestCase.java
@@ -6,12 +6,10 @@
 package org.wildfly.extension.elytron;
 
 import static org.jboss.as.model.test.FailedOperationTransformationConfig.REJECTED_RESOURCE;
-import static org.jboss.as.model.test.ModelTestControllerVersion.EAP_7_4_0;
-import static org.jboss.as.model.test.ModelTestControllerVersion.EAP_8_0_0;
 import static org.junit.Assert.assertTrue;
 import static org.wildfly.extension.elytron.ElytronDescriptionConstants.TRUST_MANAGER;
 
-import java.io.IOException;
+import java.util.EnumSet;
 import java.util.List;
 
 import org.jboss.as.controller.ModelVersion;
@@ -22,12 +20,17 @@ import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
 import org.jboss.as.model.test.FailedOperationTransformationConfig;
 import org.jboss.as.model.test.ModelTestControllerVersion;
 import org.jboss.as.model.test.ModelTestUtils;
+import org.jboss.as.subsystem.test.AbstractSubsystemTest;
 import org.jboss.as.subsystem.test.AdditionalInitialization;
 import org.jboss.as.subsystem.test.KernelServices;
 import org.jboss.as.subsystem.test.KernelServicesBuilder;
+import org.jboss.as.version.Stability;
 import org.jboss.dmr.ModelNode;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  * Tests of transformation of the elytron subsystem to previous API versions.
@@ -35,34 +38,57 @@ import org.junit.Test;
  * @author Brian Stansberry
  * @author Tomaz Cerar
  */
-public class SubsystemTransformerTestCase extends AbstractElytronSubsystemBaseTest {
+@RunWith(Parameterized.class)
+public class SubsystemTransformerTestCase extends AbstractSubsystemTest {
+
+    @Parameters
+    public static Iterable<ModelTestControllerVersion> parameters() {
+        return EnumSet.of(ModelTestControllerVersion.EAP_7_4_0, ModelTestControllerVersion.EAP_8_0_0, ModelTestControllerVersion.WILDFLY_31_0_0);
+    }
 
     private static final PathAddress SUBSYSTEM_ADDRESS = PathAddress.pathAddress(ModelDescriptionConstants.SUBSYSTEM, ElytronExtension.SUBSYSTEM_NAME);
 
-    public SubsystemTransformerTestCase() {
-        super(ElytronExtension.SUBSYSTEM_NAME, new ElytronExtension());
+    private final ModelTestControllerVersion controllerVersion;
+    private final org.jboss.as.subsystem.test.AdditionalInitialization additionalInitialization;
+    private final ModelVersion version;
+
+    public SubsystemTransformerTestCase(ModelTestControllerVersion controller) {
+        super(ElytronExtension.SUBSYSTEM_NAME, new ElytronExtension(), controller.getStability());
+        this.controllerVersion = controller;
+        this.version = this.getModelVersion();
+        this.additionalInitialization = AdditionalInitialization.fromModelTestControllerVersion(controller);
+
     }
 
-    @Override
-    protected String getSubsystemXml() throws IOException {
-        return readResource("elytron-transformers-13.0.xml");
-    }
-
-    protected String getSubsystemXml(final String subsystemFile) throws IOException {
-        return readResource(subsystemFile);
+    private ModelVersion getModelVersion() {
+        switch (this.controllerVersion) {
+            case EAP_7_4_0:
+                return ElytronExtension.ELYTRON_13_0_0;
+            case EAP_8_0_0:
+            case WILDFLY_31_0_0:
+                return ElytronExtension.ELYTRON_18_0_0;
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 
     /**
      * Test case testing resources and attributes are appropriately rejected when transforming to EAP 7.4.
      */
     @Test
-    public void testRejectingTransformersEAP740() throws Exception {
-        testRejectingTransformers(EAP_7_4_0, "elytron-transformers-13.0-reject.xml", new FailedOperationTransformationConfig()
-                .addFailedAttribute(SUBSYSTEM_ADDRESS.append(PathElement.pathElement(ElytronDescriptionConstants.SERVER_SSL_SNI_CONTEXT, "SNIwithCaret")),
+    public void testRejectingTransformers() throws Exception {
+        String subsystemXmlFile = String.format((this.controllerVersion.getStability() == Stability.DEFAULT) ? "elytron-transformers-%d.%d-reject.xml" : "elytron-transformers-%d.%d-%s-reject.xml", this.version.getMajor(), this.version.getMinor(), this.controllerVersion.getStability());
+        FailedOperationTransformationConfig config = new FailedOperationTransformationConfig();
+        if (this.version.compareTo(ElytronExtension.ELYTRON_18_0_0) >= 0) {
+            // TODO add missing rejection validation for EAP 8.0
+            if (this.controllerVersion.getStability().enables(Stability.COMMUNITY)) {
+                config.addFailedAttribute(SUBSYSTEM_ADDRESS.append(PathElement.pathElement(ElytronDescriptionConstants.DYNAMIC_CLIENT_SSL_CONTEXT, "dynamicClientSSLContext")), REJECTED_RESOURCE);
+            }
+        }
+        if (this.version.compareTo(ElytronExtension.ELYTRON_13_0_0) >= 0) {
+            config.addFailedAttribute(SUBSYSTEM_ADDRESS.append(PathElement.pathElement(ElytronDescriptionConstants.SERVER_SSL_SNI_CONTEXT, "SNIwithCaret")),
                         new FailedOperationTransformationConfig.NewAttributesConfig(ElytronDescriptionConstants.HOST_CONTEXT_MAP)
                 )
-            .addFailedAttribute(SUBSYSTEM_ADDRESS.append(PathElement.pathElement(ElytronDescriptionConstants.DYNAMIC_CLIENT_SSL_CONTEXT, "dcsc")),
-                    FailedOperationTransformationConfig.REJECTED_RESOURCE)
             .addFailedAttribute(SUBSYSTEM_ADDRESS.append(PathElement.pathElement(ElytronDescriptionConstants.PROPERTIES_REALM, "PropertiesRealmEncodingCharset")),
                     FailedOperationTransformationConfig.REJECTED_RESOURCE)
             .addFailedAttribute(SUBSYSTEM_ADDRESS.append(PathElement.pathElement(ElytronDescriptionConstants.FILESYSTEM_REALM, "FilesystemRealmEncodingCharset")),
@@ -90,32 +116,33 @@ public class SubsystemTransformerTestCase extends AbstractElytronSubsystemBaseTe
                 .addFailedAttribute(SUBSYSTEM_ADDRESS.append(PathElement.pathElement(ElytronDescriptionConstants.PERIODIC_ROTATING_FILE_AUDIT_LOG)),
                     new FailedOperationTransformationConfig.NewAttributesConfig(AuditResourceDefinitions.ENCODING))
                 .addFailedAttribute(SUBSYSTEM_ADDRESS.append(PathElement.pathElement(ElytronDescriptionConstants.SIZE_ROTATING_FILE_AUDIT_LOG)),
-                    new FailedOperationTransformationConfig.NewAttributesConfig(AuditResourceDefinitions.ENCODING))
-        );
+                    new FailedOperationTransformationConfig.NewAttributesConfig(AuditResourceDefinitions.ENCODING));
+        }
+        testRejectingTransformers(subsystemXmlFile, config);
     }
 
     /**
      * Test case testing resources and attributes are appropriately transformed when transforming to EAP 7.4.
      */
     @Test
-    public void testTransformerEAP740() throws Exception {
-        testTransformation(EAP_7_4_0);
+    public void testTransformer() throws Exception {
+        final ModelVersion version = this.controllerVersion.getSubsystemModelVersion(getMainSubsystemName());
+        String subsystemXml = this.readResource(String.format((this.controllerVersion.getStability() == Stability.DEFAULT) ? "elytron-transformers-%d.%d.xml" : "elytron-transformers-%d.%d-%s.xml", this.version.getMajor(), this.version.getMinor(), this.controllerVersion.getStability()));
+
+        KernelServices services = this.buildKernelServices(subsystemXml, version,
+                this.controllerVersion.getCoreMavenGroupId() + ":wildfly-elytron-integration:" + this.controllerVersion.getCoreVersion());
+
+        // check that both versions of the legacy model are the same and valid
+        checkSubsystemModelTransformation(services, version, null, false);
+
+        ModelNode transformed = services.readTransformedModel(version);
+        Assert.assertTrue(transformed.isDefined());
     }
 
-    /**
-     * Test case testing resources and attributes are appropriately transformed when transforming to EAP 8.0.0.
-     */
-    @Test
-    public void testTransformerEAP800() throws Exception {
-        testTransformation(EAP_8_0_0);
-    }
+    private KernelServices buildKernelServices(String xml, ModelVersion version, String... mavenResourceURLs) throws Exception {
+        KernelServicesBuilder builder = this.createKernelServicesBuilder(this.additionalInitialization).setSubsystemXml(xml);
 
-    private KernelServices buildKernelServices(String xml, ModelTestControllerVersion controllerVersion, ModelVersion version, String... mavenResourceURLs) throws Exception {
-        AdditionalInitialization additionalInitialization = AdditionalInitialization.fromModelTestControllerVersion(controllerVersion);
-
-        KernelServicesBuilder builder = this.createKernelServicesBuilder(additionalInitialization).setSubsystemXml(xml);
-
-        builder.createLegacyKernelServicesBuilder(additionalInitialization, controllerVersion, version)
+        builder.createLegacyKernelServicesBuilder(this.additionalInitialization, this.controllerVersion, version)
                 .addMavenResourceURL(mavenResourceURLs)
                 .skipReverseControllerCheck()
                 .addParentFirstClassPattern("org.jboss.as.controller.logging.ControllerLogger*")
@@ -128,36 +155,19 @@ public class SubsystemTransformerTestCase extends AbstractElytronSubsystemBaseTe
 
         KernelServices services = builder.build();
         Assert.assertTrue(ModelTestControllerVersion.MASTER + " boot failed", services.isSuccessfulBoot());
-        Assert.assertTrue(controllerVersion.getMavenGavVersion() + " boot failed", services.getLegacyServices(version).isSuccessfulBoot());
+        Assert.assertTrue(this.controllerVersion.getMavenGavVersion() + " boot failed", services.getLegacyServices(version).isSuccessfulBoot());
         return services;
     }
 
-    private void testTransformation(final ModelTestControllerVersion controller, final String subsystemXml) throws Exception {
-        final ModelVersion version = controller.getSubsystemModelVersion(getMainSubsystemName());
-
-        KernelServices services = this.buildKernelServices(subsystemXml, controller, version,
-                controller.getCoreMavenGroupId() + ":wildfly-elytron-integration:" + controller.getCoreVersion());
-
-        // check that both versions of the legacy model are the same and valid
-        checkSubsystemModelTransformation(services, version, null, false);
-
-        ModelNode transformed = services.readTransformedModel(version);
-        Assert.assertTrue(transformed.isDefined());
-    }
-
-    private void testTransformation(final ModelTestControllerVersion controller) throws Exception {
-        testTransformation(controller, getSubsystemXml());
-    }
-
-    private void testRejectingTransformers(ModelTestControllerVersion controllerVersion, final String subsystemXmlFile, final FailedOperationTransformationConfig config) throws Exception {
-        ModelVersion elytronVersion = controllerVersion.getSubsystemModelVersion(getMainSubsystemName());
+    private void testRejectingTransformers(final String subsystemXmlFile, final FailedOperationTransformationConfig config) throws Exception {
+        ModelVersion elytronVersion = this.controllerVersion.getSubsystemModelVersion(getMainSubsystemName());
 
         //Boot up empty controllers with the resources needed for the ops coming from the xml to work
-        KernelServicesBuilder builder = createKernelServicesBuilder(AdditionalInitialization.withCapabilities(controllerVersion.getStability(),
+        KernelServicesBuilder builder = createKernelServicesBuilder(AdditionalInitialization.withCapabilities(this.controllerVersion.getStability(),
                         RuntimeCapability.buildDynamicCapabilityName(Capabilities.DATA_SOURCE_CAPABILITY_NAME, "ExampleDS")
         ));
-        builder.createLegacyKernelServicesBuilder(AdditionalInitialization.MANAGEMENT, controllerVersion, elytronVersion)
-                .addMavenResourceURL(controllerVersion.getCoreMavenGroupId() + ":wildfly-elytron-integration:" + controllerVersion.getCoreVersion())
+        builder.createLegacyKernelServicesBuilder(AdditionalInitialization.MANAGEMENT, this.controllerVersion, elytronVersion)
+                .addMavenResourceURL(this.controllerVersion.getCoreMavenGroupId() + ":wildfly-elytron-integration:" + this.controllerVersion.getCoreVersion())
                 .addParentFirstClassPattern("org.jboss.as.controller.logging.ControllerLogger*")
                 .addParentFirstClassPattern("org.jboss.as.controller.PathAddress")
                 .addParentFirstClassPattern("org.jboss.as.controller.PathElement")
